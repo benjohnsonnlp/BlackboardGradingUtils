a0b6f83f6e960257cf2c42d2a507f9b928c15de1
Ben <doc.ben.johnson@gmail.com>
2019-03-09 14:01:29 -0500

    Pre lab quiz fixes

---
+++ normalize.py
@@ -0,0 +1,34 @@
+import csv
+
+
+def normalize(filename, prefix, min_value, max_value, zero_strings=["Needs Grading", "In Progress", ""], output_filename="corrected.csv"):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        header = list(next(reader))
+        output_sheet = []
+        remainder = [row for row in reader]
+        for row in remainder:
+            output_row = list()
+            for header_label, element in zip(header, row):
+                output = element
+                if header_label.startswith(prefix):
+                    try:
+                        if float(element) > max_value:
+                            output = max_value
+                    except ValueError:
+                        pass
+                    if element in zero_strings:
+                        output = min_value
+                output_row.append(output)
+
+            output_sheet.append(output_row)
+
+        with open(output_filename, 'w') as o:
+            writer = csv.writer(o, delimiter=",")
+            writer.writerow([item for item in header])
+            for row in output_sheet:
+                writer.writerow(row)
+
+
+if __name__ == '__main__':
+    normalize("grades.csv", "Pre Lab", 0, 2)
